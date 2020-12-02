75f1cf6ac39fac984fae999d27defc533b873ddc
David <davidjhinku@gmail.com>
2020-12-02 16:24:52 -0500

    00 completed

--- sql_activerecord/lib/00_attr_accessor_object.rb
+++ sql_activerecord/lib/00_attr_accessor_object.rb
@@ -1,9 +1,10 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-
+    #self attr_accessor object
+    
     names.each do |name|
       define_method(name.to_s) do
-        self.instance_variable_get("@#{name}")
+        self.instance_variable_get("@#{name}")   #self instance of att_acc object
       end
 
       define_method("#{name}=") do |value|
@@ -14,4 +15,4 @@ class AttrAccessorObject
   end
 end
 
-AttrAccessorObject.my_attr_accessor(:num1, :second2)
+AttrAccessorObject.my_attr_accessor(:x, :y, "z")
